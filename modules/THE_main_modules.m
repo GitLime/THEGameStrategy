102c62e950f1ff353c23571def72baa99e77f47b
jdlg <johandleg@gmail.com>
2015-11-10 11:57:26 +0100

    table and dealer

--- modules/THE_main_modules.m
+++ modules/THE_main_modules.m
@@ -11,13 +11,17 @@ global_info.card_dealt_counter = 4;
 global_info.end_hand = 0;
 %global_info.player_decision_states = {''}
 
-player_modules = {'smpl_player_pdf', 'smpl_player_pdf', 'smpl_player_pdf', 'smpl_player_pdf'};
+player_modules = {'dealer_pdf', 'table_pdf', 'smpl_player_pdf', 'smpl_player_pdf', 'smpl_player_pdf', 'smpl_player_pdf'};
 pdfs = {'THE_module_pdf'};
 pdfs = [player_modules pdfs];
 
+
 pns = pnstruct(pdfs);
 
-dyn.m0  = {'pDealer',8, 'pP1Turn',1};
+
+
+%dyn.m0  = {'pDealer',8, 'pP1Turn',1};
+dyn.m0  = {'pTableP1Out',1};
 
 prnsys(pns, dyn);
 
--- modules/smpl_player_pdf.m
+++ modules/smpl_player_pdf.m
@@ -6,17 +6,21 @@ function [pns] = smpl_player_pdf()
     global_info.players_index = global_info.players_index + 1;
     %we name all Ps and Ts with the player number
     
+    %tP1TurnIn
+    %pP1TurnOut
+    
     
     pns.set_of_Ps = {strcat('pP', num2str(player_nr), 'Cards'), ...
-        strcat('pP', num2str(player_nr), 'Turn')};
+        strcat('pP', num2str(player_nr), 'TurnOut')};
     %pPlayer1Cards: for holding two cards
     %pPlayer1Turn: for holding the turn token
     
     pns.set_of_Ts = {strcat('tP', num2str(player_nr), 'In'), ...
-        strcat('tP', num2str(player_nr), 'Decision')};
+        strcat('tP', num2str(player_nr), 'TurnIn')};
     %tPlayer1In: in interface from the table
     %tPlayer1Decision: fires when the player makes a play
     
     pns.set_of_As =  {...
         strcat('tP', num2str(player_nr), 'In'), strcat('pP', num2str(player_nr), 'Cards'), 2,...
-        strcat('pP', num2str(player_nr), 'Turn'), strcat('tP', num2str(player_nr), 'Decision'), 1};
+        strcat('tP', num2str(player_nr), 'TurnIn'), strcat('pP', num2str(player_nr), 'TurnOut'), 1};
+    
