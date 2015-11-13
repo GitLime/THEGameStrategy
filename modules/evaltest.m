ee81d30ef44e376d88e80525a6181def21c05528
unknown <215699@D10654.uis.no>
2015-11-13 13:42:57 +0100

    eval

--- modules/evaltest.m
+++ modules/evaltest.m
@@ -1,13 +1,21 @@
-% hands = [{'ks', 'kk'};{'ad', 'qd'};{'2k', '2h'}];
-% table = {'qs', '2d', '4h', 'tk', '3d'};
-% 
-% for i = 1:length(hands)
-%     hand = hands(i,:);
+hands = containers.Map;
+hands('1') = {'ks', 'kk'};
+hands('2') = {'ad', 'qd'};
+hands('4') = {'2k', '2h'};
+hands('6') = {'4k', '4h'};
+table = {'qs', '2d', '4h', 'tk', '3d'};
+
+% for hand = hands.values
 %     cards = [hand table];
 % end 
 
-cards = {'9s' 'ts' 'js' '5k' '5s' 'qs' 'ks'};
-cards = {'8s' '2s' 'ts' '9d' '5s' 'ad' 'ad'};
-evaluate_hand(cards)
+for k = hands.keys
+    hand = hands(char(k));
+    cards = [hand table]
+end 
 
-%evaluate(gands, table)
+% cards = {'9s' 'ts' 'js' '5k' '5s' 'qs' 'ks'};
+% cards = {'8s' '2s' 'ts' '9d' '5s' 'ad' 'ad'};
+% evaluate_hand(cards)
+
+% evaluate(hands, table)
--- modules/evaluate.m
+++ modules/evaluate.m
@@ -1,6 +1,26 @@
-function [ outcome ] = evaluate( hands, table )
+function [ winners ] = evaluate( hands, table )
+    %foreach hand: make a 7 card set and evaluate it
+    hand_types = [];
+    hand_cards = [];
+    hands = containers.Map;
+    for k = hands.keys
+        hand = hands(char(k));
+        cards = [hand table];
+        eval = evaluate_hand(cards);
+        hand_types = [hand_types eval.value];
+        hands(char(k)) = eval;
+    end 
+    m = max(hand_types);
+    top_hands = find(hand_types == m);
+    if length(top_hands) == 1
+        winners = top_hands;
+        return
+    end
+    top_hands = [];
+    for hand = hands
+        top_hands =[top_hands; hand.hand]
+    end
     
-
 
 end
 
