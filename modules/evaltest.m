a72087d4ed28c8dab2dfca0848bfdfe111cb4fe8
unknown <215699@D10653.uis.no>
2015-11-13 14:42:49 +0100

    eval done

--- modules/evaltest.m
+++ modules/evaltest.m
@@ -3,7 +3,7 @@ hands('1') = {'ks', 'kk'};
 hands('2') = {'ad', 'qd'};
 hands('4') = {'2k', '2h'};
 hands('6') = {'4k', '4h'};
-table = {'qs', '2d', '4h', 'tk', '3d'};
+table = {'qs', 'td', 'ah', 'jk', '3d'};
 
 % for hand = hands.values
 %     cards = [hand table];
@@ -11,11 +11,11 @@ table = {'qs', '2d', '4h', 'tk', '3d'};
 
 for k = hands.keys
     hand = hands(char(k));
-    cards = [hand table]
+    cards = [hand table];
 end 
 
 % cards = {'9s' 'ts' 'js' '5k' '5s' 'qs' 'ks'};
 % cards = {'8s' '2s' 'ts' '9d' '5s' 'ad' 'ad'};
 % evaluate_hand(cards)
 
-% evaluate(hands, table)
+ evaluate(hands, table)
